a6592b8f08a0c13a63d457a15f1955aa4b708e90
gaelgael <gaelgael5@gmail.com>
2019-10-29 10:48:31 +0100

    initialize solution

---
+++ Src/Black.Beard.ComponentModel.Attributes/Attributes/RegisterMethodAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bb.ComponentModel.Attributes
+{
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public class ExposeMethodAttribute : Attribute
+    {
+
+        public ExposeMethodAttribute()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="displayName">key for matching ruless</param>
+        public ExposeMethodAttribute(string displayName)
+        {
+            this.DisplayName = displayName;
+        }
+
+        public string DisplayName { get; set; }
+
+        public string Context { get; set; }
+
+    }
+
+}
