4297f8f164aafa19a000d942af9c43d90f261ca9
gael beard <g.beard@pickup-services.com>
2022-01-26 08:10:11 +0100

    add can execute on IInjectBuilder

--- Src/Black.Beard.ComponentModel.Attributes/ComponentModel/IInjectBuilder.cs
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/IInjectBuilder.cs
@@ -8,13 +8,28 @@ namespace Bb.ComponentModel
 {
     
     /// <summary>
-    /// 
+    /// Generic interface for Initialize service
     /// </summary>
     public interface IInjectBuilder
     {
 
+        /// <summary>
+        /// Execute the initializing process with <see cref="object"/>
+        /// </summary>
+        /// <param name="context">specified context <see cref="object"/></param>
+        /// <returns></returns>
         object Run(object context);
 
+        /// <summary>
+        /// Return true if the process can be run
+        /// </summary>
+        /// <param name="context">specified context <see cref="object"/></param>
+        /// <returns></returns>
+        bool CanExecute(object context);
+
+        /// <summary>
+        /// Return the type of service that should be passed by argument
+        /// </summary>
         Type Type { get; }
 
 
--- Src/Black.Beard.ComponentModel.Attributes/ComponentModel/IInjectBuilder1.cs
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/IInjectBuilder1.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace Bb.ComponentModel
 {
-    
 
+    /// <summary>
+    /// specialized interface for Initialize service
+    /// </summary>
     public interface IInjectBuilder<T> : IInjectBuilder
     {
 
+        /// <summary>
+        /// Execute the initializing process with <see cref="T"/>
+        /// </summary>
+        /// <param name="context">specified context <see cref="T"/></param>
+        /// <returns></returns>
         object Run(T context);
+
+        /// <summary>
+        /// Return true if the process can be run
+        /// </summary>
+        /// <param name="context">specified context <see cref="T"/></param>
+        /// <returns></returns>
+        bool CanExecute(T context);
 
 
     }
