8bdefe77df7be54aab6540e6ec6ca71d0f9c19ae
gael beard <g.beard@pickup-services.com>
2022-02-08 08:42:54 +0100

    Add OrderDisplay Attribute

--- Src/Black.Beard.ComponentModel.Attributes/ComponentModel/DataAnnotations/DisplayOnUITextAreaAttribute.cs
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/DataAnnotations/DisplayOnUITextAreaAttribute.cs
@@ -3,6 +3,7 @@
 namespace Bb.ComponentModel.DataAnnotations
 {
 
+
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
 
     public sealed class DisplayOnUITextAreaAttribute : Attribute
---
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/DataAnnotations/OrderDisplayAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bb.ComponentModel.DataAnnotations
+{
+    [System.AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Parameter, Inherited = false, AllowMultiple = true)]
+    public sealed class OrderDisplayAttribute : Attribute
+    {
+        
+        public OrderDisplayAttribute(int position)
+        {
+            this.Position = position;
+        }
+
+        public int Position { get; }
+
+    }
+
+}
