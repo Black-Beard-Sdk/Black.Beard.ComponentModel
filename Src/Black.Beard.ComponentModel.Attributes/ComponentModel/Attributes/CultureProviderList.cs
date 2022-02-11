b95b956dbc0288ee56460ba93afdcb9f255b10da
gael beard <g.beard@pickup-services.com>
2022-02-11 11:01:53 +0100

    Add cultureProviderList

---
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/Attributes/CultureProviderList.cs
@@ -0,0 +1,28 @@
+﻿using Bb.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Bb.ComponentModel.Attributes
+{
+    public class CultureProviderList : IListProvider
+    {
+
+
+        public PropertyDescriptor Property { get; set; }
+
+
+        public object Instance { get; set; }
+
+
+        public IEnumerable<ListItem> GetItems()
+        {
+            var items = CultureInfo.GetCultures(CultureTypes.FrameworkCultures);
+            foreach (var item in items)
+                yield return new ListItem() { Display = item.DisplayName, Name = item.EnglishName, Value = item.IetfLanguageTag };
+
+        }
+
+    }
+
+}
