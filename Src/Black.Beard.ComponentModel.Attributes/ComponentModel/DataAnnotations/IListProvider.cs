db1c74cc08a19c39e75a174b7745f244af2a046c
gael beard <gaelgael5@gmail.com>
2024-04-08 19:04:26 +0200

    Add comment on attributes

--- Src/Black.Beard.ComponentModel.Attributes/ComponentModel/DataAnnotations/IListProvider.cs
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/DataAnnotations/IListProvider.cs
@@ -5,13 +5,33 @@ using System.ComponentModel;
 
 namespace Bb.ComponentModel.DataAnnotations
 {
+
+
+    /// <summary>
+    /// Provides a list of items for a property.
+    /// </summary>
     public interface IListProvider
     {
 
+        /// <summary>
+        /// Gets or sets the service provider.
+        /// </summary>
+        IServiceProvider ServiceProvider { get; set; }
+
+        /// <summary>
+        /// Gets or sets the instance. for th property
+        /// </summary>
         object Instance { get; set; }
 
+        /// <summary>
+        /// Gets or sets the property descriptor.
+        /// </summary>
         PropertyDescriptor Property { get; set; }
                 
+        /// <summary>
+        /// Gets the items list.
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<ListItem> GetItems();
 
     }
--- Src/Black.Beard.ComponentModel.Attributes/ComponentModel/DataAnnotations/ListItem.cs
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/DataAnnotations/ListItem.cs
@@ -1,25 +1,51 @@
 ﻿namespace Bb.ComponentModel.DataAnnotations
 {
+
+    /// <summary>
+    /// Represents a list item.
+    /// </summary>
     public class ListItem
     {
 
+        /// <summary>
+        /// Gets or sets the name. (it is the key)
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the display.
+        /// </summary>
         public string Display { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
         public object Value { get; set; }
 
+        /// <summary>
+        /// Gets or sets the string display
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return Display.ToString();
         }
 
+        /// <summary>
+        /// retur true if the value is equal to the value of the object
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
         public override bool Equals(object o)
         {
             var other = o as ListItem;
             return other?.Value == Value;
         }
 
+        /// <summary>
+        /// return the hash code of the value
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
             return Value.GetHashCode();
