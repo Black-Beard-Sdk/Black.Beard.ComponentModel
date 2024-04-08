8978ee37bdb9a5ea993a801a072d98d08d28851b
gael beard <gaelgael5@gmail.com>
2024-04-08 19:47:15 +0200

    Fix implementation of culture

--- Src/Black.Beard.ComponentModel.Attributes/ComponentModel/Attributes/CultureProviderList.cs
+++ Src/Black.Beard.ComponentModel.Attributes/ComponentModel/Attributes/CultureProviderList.cs
@@ -1,4 +1,5 @@
 ﻿using Bb.ComponentModel.DataAnnotations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
@@ -8,13 +9,25 @@ namespace Bb.ComponentModel.Attributes
     public class CultureProviderList : IListProvider
     {
 
-
+        /// <summary>
+        /// Property descriptor
+        /// </summary>
         public PropertyDescriptor Property { get; set; }
 
-
+        /// <summary>
+        /// Instance of the object that contains the property
+        /// </summary>
         public object Instance { get; set; }
 
+        /// <summary>
+        /// Service provider
+        /// </summary>
+        public IServiceProvider ServiceProvider { get; set; }
 
+        /// <summary>
+        /// Get the list of items
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<ListItem> GetItems()
         {
             var items = CultureInfo.GetCultures(CultureTypes.FrameworkCultures);
