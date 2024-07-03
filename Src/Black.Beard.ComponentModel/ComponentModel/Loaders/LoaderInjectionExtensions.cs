296ea1b9d03d7fc16182a531e316cb9744ad9f35
gael beard <gaelgael5@gmail.com>
2024-07-03 08:27:34 +0200

    Add initializing service

--- Src/Black.Beard.ComponentModel/ComponentModel/Loaders/LoaderInjectionExtensions.cs
+++ Src/Black.Beard.ComponentModel/ComponentModel/Loaders/LoaderInjectionExtensions.cs
@@ -16,6 +16,26 @@ namespace Bb.ComponentModel.Loaders
 
 
         /// <summary>
+        /// Initialize the specified instance with classes that will be discovered
+        /// </summary>
+        /// <typeparam name="T">Type of object must by resolved by discovery</typeparam>
+        /// <param name="self">instance to initialize</param>
+        /// <param name="serviceProvider"><see cref="IServiceProvider"/></param>
+        /// <param name="context">by default the value is "Initialization"</param>
+        /// <param name="action">action to execute for every loader</param>
+        /// <returns></returns>
+        public static T Initialize<T>(this T self, IServiceProvider serviceProvider, string? context = null, Action<IInjectBuilder<T>> action = null)
+        {
+
+            var loader = new InjectionLoader<T>(context ?? ConstantsCore.Initialization, serviceProvider)
+                .LoadModules(action)
+                .Execute(self);
+
+            return self;
+
+        }
+
+        /// <summary>
         /// create instance and initialize service from service provider
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -23,14 +43,14 @@ namespace Bb.ComponentModel.Loaders
         /// <param name="context"></param>
         /// <param name="initializer"></param>
         /// <returns></returns>
-        public static T GetInitializedService<T>(this IServiceProvider serviceProvider, string context, Action<InjectionLoader<T>> initializer = null)
+        public static T GetInitializedService<T>(this IServiceProvider serviceProvider, string context, Action<IInjectBuilder<T>> initializer = null)
         {
 
             var instance = (T)serviceProvider.GetService(typeof(T));
             if (instance != null)
             {
                 var loader = new InjectionLoader<T>(context, serviceProvider)
-                    .LoadModules()
+                    .LoadModules(initializer)
                     .Execute(instance)
                     ;
 
